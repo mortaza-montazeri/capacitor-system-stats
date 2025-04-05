75487e12589c389f491f7b9263c1d8be58a6e357
Mortaza <mortaza.gh@gmail.com>
2025-04-05 15:52:14 +0330

    change cpu usage calculation

--- android/src/main/java/com/netacoltd/plugins/systemstats/SystemStatsPlugin.java
+++ android/src/main/java/com/netacoltd/plugins/systemstats/SystemStatsPlugin.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.os.Environment;
 import android.os.StatFs;
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.getcapacitor.JSObject;
@@ -26,17 +27,34 @@ public class SystemStatsPlugin extends Plugin {
         
         // CPU Usage (Approximation)
         try {
+            // Execute the top command and get the output
             Process process = Runtime.getRuntime().exec("top -n 1");
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
-            String cpuUsage = null;
+            String cpuLine = null;
+            float cpuUsage;
+
+            // Find the line containing the CPU usage data
             while ((line = reader.readLine()) != null) {
-                if (line.contains("CPU")) {
-                    cpuUsage = line;
+                if (line.contains("User")) { // This will depend on the device, look for the CPU usage line
+                    cpuLine = line;
                     break;
                 }
             }
-            ret.put("cpuUsage", cpuUsage);
+            reader.close();
+
+            if (cpuLine != null) {
+                // Example line: "User     0.0%  System    0.0%  Idle    99.9%"
+                // Split the line into separate parts
+                String[] parts = cpuLine.split("\\s+");
+                float userCpu = parseCpuPercentage(parts[1]); // 0.0%
+                float systemCpu = parseCpuPercentage(parts[3]); // 0.0%
+                // float idleCpu = parseCpuPercentage(parts[5]); // 99.9%
+
+                ret.put("cpuUsage", userCpu + systemCpu);
+            } else {
+                ret.put("cpuUsage", null);
+            }
         } catch (Exception e) {
             Log.e("SystemStatsPlugin", "getSystemStats: ", e);
             ret.put("cpuUsage", null);
